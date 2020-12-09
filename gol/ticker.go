03a61498d5292c22c64059a541517cc1fac6403f
Stefanos <wz19702@bristol.ac.uk>
2020-12-09 17:20:00 +0000

    simplified ticker yet not functional

--- gol/ticker.go
+++ gol/ticker.go
@@ -2,31 +2,23 @@ package gol
 
 import (
 	"time"
-	"uk.ac.bris.cs/gameoflife/util"
 )
 
 type TickerChans struct {
-	turnChan    chan int
-	done 		chan bool
-
+	tick chan bool
+	done chan bool
 }
 
 // ticker function
-func(t *TickerChans) ticker(c distributorChannels, aliveCells []util.Cell, done <-chan bool) {
+func (t *TickerChans) ticker(done <-chan bool) {
 	ticker := time.NewTicker(2 * time.Second)
-	turn := 0
-	running := true
-	for running {
-		select {
-		case <-ticker.C:
-			c.events <- AliveCellsCount{turn, len(aliveCells)}
-		case <-t.done:
-			ticker.Stop()
-			running = false
-		case currentTurns := <-t.turnChan:
-			turn = currentTurns + 1
-		default:
-			break
-		}
+
+	select {
+	case <-ticker.C:
+		t.tick <- true
+	case <-t.done:
+		ticker.Stop()
+
 	}
+
 }
