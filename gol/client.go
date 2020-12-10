57c3a0cc40c636d04f84528c7da5dd5514f1d61f
Stefanos <wz19702@bristol.ac.uk>
2020-12-10 15:02:48 +0000

    transfered critical methods to client from distributor

--- gol/client.go
+++ gol/client.go
@@ -45,59 +45,149 @@ func calculateAliveClient(p Params, world [][]byte) []util.Cell {
 	return aliveCells
 }
 
+//progress to next state and update CellFlipped event
+func calculateNextState(p Params, turn int, c ClientChans, world [][]byte) [][]byte {
+	newWorld := make([][]byte, p.ImageHeight)
+	for i := range newWorld {
+		newWorld[i] = make([]byte, p.ImageWidth)
+	}
+	for y := 0; y < p.ImageHeight; y++ {
+		for x := 0; x < p.ImageWidth; x++ {
+			neighbours := calculateNeighbours(p, x, y, world)
+			if world[y][x] == alive {
+				if neighbours == 2 || neighbours == 3 {
+					newWorld[y][x] = alive
+				} else {
+					newWorld[y][x] = dead
+					c.events <- CellFlipped{turn, util.Cell{X: x, Y: y}}
+				}
+			} else {
+				if neighbours == 3 {
+					newWorld[y][x] = alive
+					c.events <- CellFlipped{turn, util.Cell{X: x, Y: y}}
+				} else {
+					newWorld[y][x] = dead
+				}
+			}
+		}
+	}
+	return newWorld
+}
+
+//calculateNeighbors takes the current state of the world and completes one evolution of the world. It then returns the result
+func calculateNeighbours(p Params, x, y int, world [][]byte) int {
+	neighbours := 0
+	for i := -1; i <= 1; i++ {
+		for j := -1; j <= 1; j++ {
+			if i != 0 || j != 0 {
+				if world[mod(y+i, p.ImageHeight)][mod(x+j, p.ImageWidth)] == alive {
+					neighbours++
+				}
+			}
+		}
+	}
+	return neighbours
+}
+
+// creates a grid for the current state of the world
+func makeWorld(height int, width int, c ClientChans) [][]uint8 {
+	world := make([][]uint8, height)
+	for row := 0; row < height; row++ {
+		world[row] = make([]uint8, width)
+		for cell := 0; cell < width; cell++ {
+			world[row][cell] = <-c.ioInput
+		}
+	}
+	return world
+}
+
 // pause the game
-// func pause(c ClientChans, turn int, x rune) {
-// 	c.events <- StateChange{turn, Paused}
-// 	fmt.Println("The current turn is being processed.")
-// 	x = ' '
-// 	for x != 'p' {
-// 		x = <-c.keyPresses
-// 	}
-// 	fmt.Println("Continuing")
-// 	c.events <- StateChange{turn, Executing}
-// }
+func pause(c ClientChans, turn int, x rune) {
+	c.events <- StateChange{turn, Paused}
+	fmt.Println("The current turn is being processed.")
+	x = ' '
+	for x != 'p' {
+		x = <-c.keyPresses
+	}
+	fmt.Println("Continuing")
+	c.events <- StateChange{turn, Executing}
+}
 
-// func keyControl(c ClientChans, p Params, turn int, quit bool, world [][]uint8) bool {
-// 	//s to save, q to quit, p to pause/unpause, k to stop all comms with server
-// 	select {
-// 	case x := <-c.keyPresses:
-// 		if x == 's' {
-// 			saveWorld(c, p, turn, world)
-// 		} else if x == 'q' {
-// 			quit = true
-// 			c.events <- StateChange{turn, Quitting}
-// 		} else if x == 'p' {
-// 			pause(c, turn, x)
-// 		} else if x == 'k' {
+func keyControl(c ClientChans, p Params, turn int, quit bool, world [][]uint8) bool {
+	//s to save, q to quit, p to pause/unpause, k to stop all comms with server
+	select {
+	case x := <-c.keyPresses:
+		if x == 's' {
+			saveWorld(c, p, turn, world)
+		} else if x == 'q' {
+			quit = true
+			c.events <- StateChange{turn, Quitting}
+		} else if x == 'p' {
+			pause(c, turn, x)
+		} else if x == 'k' {
 
-// 		} else {
-// 			New("Wrong Key. Please press 's' to save, 'q' to quit, 'p' to pause/unpause, 'k' to close all server communications ")
-// 		}
-// 	default:
-// 		break
-// 	}
-// 	return quit
-// }
+		} else {
+			New("Wrong Key. Please press 's' to save, 'q' to quit, 'p' to pause/unpause, 'k' to close all server communications ")
+		}
+	default:
+		break
+	}
+	return quit
+}
 
-// func saveWorld(c ClientChans, p Params, turn int, world [][]uint8) {
-// 	c.ioCommand <- ioOutput
-// 	outputFilename := fmt.Sprintf("%vx%vx%v", p.ImageWidth, p.ImageHeight, turn)
-// 	c.ioFilename <- outputFilename
-// 	for row := 0; row < p.ImageHeight; row++ {
-// 		for cell := 0; cell < p.ImageWidth; cell++ {
-// 			c.ioOutput <- world[row][cell]
-// 		}
-// 	}
-// }
+func saveWorld(c ClientChans, p Params, turn int, world [][]uint8) {
+	c.ioCommand <- ioOutput
+	outputFilename := fmt.Sprintf("%vx%vx%v", p.ImageWidth, p.ImageHeight, turn)
+	c.ioFilename <- outputFilename
+	for row := 0; row < p.ImageHeight; row++ {
+		for cell := 0; cell < p.ImageWidth; cell++ {
+			c.ioOutput <- world[row][cell]
+		}
+	}
+}
 
-// func read(conn *net.Conn) {
-// 	// In a continuous loop, read a message from the server and display it.
-// 	for {
-// 		reader := bufio.NewReader(*conn)
-// 		msg, _ := reader.ReadString('\n')
-// 		fmt.Printf(msg)
-// 	}
-// }
+func gameExecution(c ClientChans, p Params) {
+
+	//Initialization
+	world := makeWorld(p.ImageHeight, p.ImageWidth, c)
+	newWorld := makeNewWorld(p.ImageHeight, p.ImageWidth)
+
+	// A variable to store current alive cells
+	aliveCells := make([]util.Cell, 0)
+	// For all initially alive cells send a CellFlipped Event.
+	c.events <- CellFlipped{0, util.Cell{X: 0, Y: 0}}
+
+	//Game of Life.
+	quit := false
+	var turn int
+
+	for turn = 0; turn < p.Turns && quit == false; turn++ {
+		
+		// Waiting all workers to finish each round
+		// for _, w := range workers {
+		// 	w.space.Wait()
+		// }
+
+		quit = keyControl(c, p, turn, quit, world)
+		newWorld = calculateNextState(p, turn, c, world)
+		aliveCells = calculateAliveCells(p, world)
+		//we add the newly updated world to the grid we had made
+		world = newWorld
+		newWorld = makeNewWorld(p.ImageHeight, p.ImageWidth)
+
+		//update events
+		c.events <- AliveCellsCount{turn, len(aliveCells)}
+		c.events <- TurnComplete{turn}
+
+		// Workers to start the next round if no q is pressed
+		// for j := 0; j < p.Threads && quit == false; j++ {
+		// 	workers[j].work.Post()
+		// }
+
+	}
+	//terminate ticker
+	// t.done <- true
+}
 
 func clientRun(p Params, c ClientChans) {
 
@@ -117,8 +207,17 @@ func clientRun(p Params, c ClientChans) {
 	c.events <- StateChange{turn, Quitting}
 
 	// // Close the channel to stop the SDL goroutine gracefully. Removing may cause deadlock.
-	// close(c.events)
+	close(c.events)
 
 	//TODO Start asynchronously reading and displaying messages
 	//TODO Start getting and sending user messages.
 }
+
+// func read(conn *net.Conn) {
+// 	// In a continuous loop, read a message from the server and display it.
+// 	for {
+// 		reader := bufio.NewReader(*conn)
+// 		msg, _ := reader.ReadString('\n')
+// 		fmt.Printf(msg)
+// 	}
+// }
--- gol/distributor.go
+++ gol/distributor.go
@@ -154,51 +154,6 @@ func (w *worker)runWorkers(p Params, c distributorChannels, world [][]uint8) {
 	}
 }
 
-func saveWorld(c distributorChannels, p Params, turn int, world [][]uint8) {
-	c.ioCommand <- ioOutput
-	outputFilename := fmt.Sprintf("%vx%vx%v", p.ImageWidth, p.ImageHeight, turn)
-	c.ioFilename <- outputFilename
-	for row := 0; row < p.ImageHeight; row++ {
-		for cell := 0; cell < p.ImageWidth; cell++ {
-			c.ioOutput <- world[row][cell]
-		}
-	}
-}
-
-// pause the game
-func pause(c distributorChannels, turn int, x rune) {
-	c.events <- StateChange{turn, Paused}
-	fmt.Println("The current turn is being processed.")
-	x = ' '
-	for x != 'p' {
-		x = <-c.keyPresses
-	}
-	fmt.Println("Continuing")
-	c.events <- StateChange{turn, Executing}
-}
-
-// Button control
-func keyControl(c distributorChannels, p Params, turn int, quit bool, world [][]uint8) bool {
-	//s to save, q to quit, p to pause/unpause, k to stop all comms with server
-	select {
-	case x := <-c.keyPresses:
-		if x == 's' {
-			saveWorld(c, p, turn, world)
-		} else if x == 'q' {
-			quit = true
-			c.events <- StateChange{turn, Quitting}
-		} else if x == 'p' {
-			pause(c, turn, x)
-		} else if x == 'k' {
-
-		} else {
-			New("Wrong Key. Please press 's' to save, 'q' to quit, 'p' to pause/unpause, 'k' to close all server communications ")
-		}
-	default:
-		break
-	}
-	return quit
-}
 
 // distributor divides the work between workers and interacts with other goroutines.
 func (w *worker)distributor(p Params, c distributorChannels) {
